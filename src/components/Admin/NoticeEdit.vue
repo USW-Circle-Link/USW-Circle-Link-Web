b126396e8499d67892873702d251b548fca34210
leedongsooo <dlehdtn0118@naver.com>
2024-10-04 00:02:45 +0900

    noticededit

--- src/components/Admin/NoticeEdit.vue
+++ src/components/Admin/NoticeEdit.vue
@@ -217,7 +217,6 @@ export default {
 
         form.append('request', new Blob([JSON.stringify(noticeData)], { type: 'application/json' }));
 
-        // eslint-disable-next-line no-unused-vars
         this.noticePhotos.forEach((image, index) => {
           if (image.file) {
             form.append('photos', image.file); // 파일이 있는 경우에만 업로드
@@ -239,7 +238,6 @@ export default {
           await Promise.all(response.data.data.noticePhotos.map(async (photoUrl, index) => {
             if (this.noticePhotos[index] && this.noticePhotos[index].file) {
               try {
-                // eslint-disable-next-line no-unused-vars
                 const photoResponse = await axios.put(photoUrl, this.noticePhotos[index].file, {
                   headers: {
                     'Content-Type': this.noticePhotos[index].file.type,
