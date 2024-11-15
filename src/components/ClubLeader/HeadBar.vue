aa831011d5d2b050a695bed19cb00f32e8b979a7
SungJae <88194064+SungJae01@users.noreply.github.com>
2024-11-15 21:48:46 +0900

    코드 정리

--- src/components/ClubLeader/HeadBar.vue
+++ src/components/ClubLeader/HeadBar.vue
@@ -2,24 +2,22 @@
     <div class="header">
         <img @click="navigateTo('dashboard')" src="../../assets/dongurami_logo.png" alt="Dongurami Logo" class="logo" />
         <h1 @click="navigateTo('dashboard')" class="title">동구라미</h1>
-        <NotificationButton/>
     </div>
 </template>
 
 <script>
-import NotificationButton from "@/components/ClubLeader/NotificationButton.vue";
 
 export default {
-name: 'Headbar',
-components: {NotificationButton},
+    name: 'Headbar',  // 구성요소 이름
     methods : {
-    navigateTo(routeName) {
-      this.$router.push({ name: routeName }).catch(err => {
-        if (err.name !== 'NavigationDuplicated') {
-          throw err;
-        }
-      });
-    },
+      // 함수 실행 시 routeName의 컴포넌트로 이동
+      navigateTo(routeName) {
+        this.$router.push({ name: routeName }).catch(err => {
+          if (err.name !== 'NavigationDuplicated') {
+            throw err;
+          }
+        });
+      },
   }
 };
 
@@ -36,7 +34,7 @@ components: {NotificationButton},
   align-items: center;
   height: 86px;
   background: #fff;
-  box-shadow: 0px 2.5px 0px 0px rgba(0, 0, 0, 0.13);
+  box-shadow: 0 2.5px 0 0 rgba(0, 0, 0, 0.13);
   margin-bottom: 40px;
   position: fixed;
   top: 0;
@@ -51,8 +49,7 @@ components: {NotificationButton},
   padding: 5px;
   transform: scaleX(-1);
   margin-left: 260px;
-  cursor: pointer; 
-
+  cursor: pointer; /* Add this line */
 }
 
 .title {
@@ -62,6 +59,6 @@ components: {NotificationButton},
   font-style: normal;
   font-weight: 405;
   line-height: 14px; /* 100% */
-  cursor: pointer; 
+  cursor: pointer; /* Add this line */
 }
 </style>
