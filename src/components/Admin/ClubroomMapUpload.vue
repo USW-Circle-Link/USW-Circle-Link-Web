95d2ad2e269e78dd31280165bb4caceffe7e7a1a
gaeng1104 <152151522+gaeng1104@users.noreply.github.com>
2025-02-05 22:51:20 +0900

    동방 위치 업로드 백엔드 연동

--- src/components/Admin/ClubroomMapUpload.vue
+++ src/components/Admin/ClubroomMapUpload.vue
@@ -1,34 +1,46 @@
 <template>
-  <div class="title">카테고리 작성하기</div>
+  <div class="title">동아리 위치 정보 수정</div>
 
   <div class="floor-images-container">
     <!-- 지하 1층 -->
     <div class="floor-section">
       <h3>지하 1층</h3>
-      <div class="image-container" @mouseover="showIcons(0, true)" @mouseleave="showIcons(0, false)">
+      <div
+        class="image-container"
+        @mouseover="showIcons('B1', true)"
+        @mouseleave="showIcons('B1', false)"
+      >
         <div class="image-wrapper">
-          <div class="image-box" :class="{ 'hovered': showingIcons[0] }">
-            <img 
-              v-if="floors[0].imageSrc" 
-              :src="floors[0].imageSrc" 
-              alt="Floor Image" 
+          <div class="image-box" :class="{ hovered: floorData.B1.showingIcon }">
+            <img
+              v-if="floorData.B1.imageSrc"
+              :src="floorData.B1.imageSrc"
+              alt="Floor Image"
               class="floor-image"
             />
-            <div v-if="showingIcons[0]" class="icons-container">
-              <img src="../../assets/zoom-in.png" class="icon zoom-icon" @click="enlargeImage(floors[0].imageSrc)" />
-              <img src="../../assets/remove.png" class="icon remove-icon" @click="deleteImage(0)" />
+            <div v-if="floorData.B1.showingIcon" class="icons-container">
+              <img
+                src="../../assets/zoom-in.png"
+                class="icon zoom-icon"
+                @click="enlargeImage(floorData.B1.imageSrc)"
+              />
+              <img
+                src="../../assets/remove.png"
+                class="icon remove-icon"
+                @click="markImageForDeletion('B1')"
+              />
             </div>
           </div>
           <div class="upload-placeholder">
-            <button @click="triggerFileInput(0)" class="upload-button">
+            <button @click="triggerFileInput('B1')" class="upload-button">
               <i class="upload-icon"></i>
               이미지 수정
             </button>
           </div>
-          <input 
-            type="file" 
-            ref="fileInput0" 
-            @change="onImageUpload(0, $event)" 
+          <input
+            type="file"
+            ref="fileInputB1"
+            @change="onImageUpload('B1', $event)"
             style="display: none;"
           />
         </div>
@@ -37,64 +49,90 @@
 
     <!-- 1층과 2층 -->
     <div class="floor-section horizontal-layout">
+      <!-- 1층 -->
       <div class="floor-container">
         <h3>1층</h3>
-        <div class="image-container" @mouseover="showIcons(1, true)" @mouseleave="showIcons(1, false)">
+        <div
+          class="image-container"
+          @mouseover="showIcons('F1', true)"
+          @mouseleave="showIcons('F1', false)"
+        >
           <div class="image-wrapper">
-            <div class="image-box" :class="{ 'hovered': showingIcons[1] }">
-              <img 
-                v-if="floors[1].imageSrc" 
-                :src="floors[1].imageSrc" 
-                alt="Floor Image" 
+            <div class="image-box" :class="{ hovered: floorData.F1.showingIcon }">
+              <img
+                v-if="floorData.F1.imageSrc"
+                :src="floorData.F1.imageSrc"
+                alt="Floor Image"
                 class="floor-image"
               />
-              <div v-if="showingIcons[1]" class="icons-container">
-                <img src="../../assets/zoom-in.png" class="icon zoom-icon" @click="enlargeImage(floors[1].imageSrc)" />
-                <img src="../../assets/remove.png" class="icon remove-icon" @click="deleteImage(1)" />
+              <div v-if="floorData.F1.showingIcon" class="icons-container">
+                <img
+                  src="../../assets/zoom-in.png"
+                  class="icon zoom-icon"
+                  @click="enlargeImage(floorData.F1.imageSrc)"
+                />
+                <img
+                  src="../../assets/remove.png"
+                  class="icon remove-icon"
+                  @click="markImageForDeletion('F1')"
+                />
               </div>
             </div>
             <div class="upload-placeholder">
-              <button @click="triggerFileInput(1)" class="upload-button">
+              <button @click="triggerFileInput('F1')" class="upload-button">
                 <i class="upload-icon"></i>
                 이미지 수정
               </button>
             </div>
-            <input 
-              type="file" 
-              ref="fileInput1" 
-              @change="onImageUpload(1, $event)" 
+            <input
+              type="file"
+              ref="fileInputF1"
+              @change="onImageUpload('F1', $event)"
               style="display: none;"
             />
           </div>
         </div>
       </div>
 
+      <!-- 2층 -->
       <div class="floor-container">
         <h3>2층</h3>
-        <div class="image-container" @mouseover="showIcons(2, true)" @mouseleave="showIcons(2, false)">
+        <div
+          class="image-container"
+          @mouseover="showIcons('F2', true)"
+          @mouseleave="showIcons('F2', false)"
+        >
           <div class="image-wrapper">
-            <div class="image-box" :class="{ 'hovered': showingIcons[2] }">
-              <img 
-                v-if="floors[2].imageSrc" 
-                :src="floors[2].imageSrc" 
-                alt="Floor Image" 
+            <div class="image-box" :class="{ hovered: floorData.F2.showingIcon }">
+              <img
+                v-if="floorData.F2.imageSrc"
+                :src="floorData.F2.imageSrc"
+                alt="Floor Image"
                 class="floor-image"
               />
-              <div v-if="showingIcons[2]" class="icons-container">
-                <img src="../../assets/zoom-in.png" class="icon zoom-icon" @click="enlargeImage(floors[2].imageSrc)" />
-                <img src="../../assets/remove.png" class="icon remove-icon" @click="deleteImage(2)" />
+              <div v-if="floorData.F2.showingIcon" class="icons-container">
+                <img
+                  src="../../assets/zoom-in.png"
+                  class="icon zoom-icon"
+                  @click="enlargeImage(floorData.F2.imageSrc)"
+                />
+                <img
+                  src="../../assets/remove.png"
+                  class="icon remove-icon"
+                  @click="markImageForDeletion('F2')"
+                />
               </div>
             </div>
             <div class="upload-placeholder">
-              <button @click="triggerFileInput(2)" class="upload-button">
+              <button @click="triggerFileInput('F2')" class="upload-button">
                 <i class="upload-icon"></i>
                 이미지 수정
               </button>
             </div>
-            <input 
-              type="file" 
-              ref="fileInput2" 
-              @change="onImageUpload(2, $event)" 
+            <input
+              type="file"
+              ref="fileInputF2"
+              @change="onImageUpload('F2', $event)"
               style="display: none;"
             />
           </div>
@@ -107,17 +145,13 @@
     <!-- 확대된 이미지 모달 -->
     <div v-if="enlargedImage" class="image-modal">
       <div class="image-wrapper">
-        <img 
-          src="../../assets/remove.png" 
-          class="remove-icon-modal" 
-          @click="closeModal" 
+        <img
+          src="../../assets/remove.png"
+          class="remove-icon-modal"
+          @click="closeModal"
           alt="Close Modal"
         />
-        <img 
-          :src="enlargedImage" 
-          alt="Enlarged Image" 
-          class="enlarged-image" 
-        />
+        <img :src="enlargedImage" alt="Enlarged Image" class="enlarged-image" />
       </div>
     </div>
   </div>
@@ -125,55 +159,68 @@
 
 <script>
 import axios from 'axios';
+import store from '../../store/store';
 
 export default {
   data() {
     return {
-      floors: [
-        { imageSrc: '', file: null }, // 지하 1층
-        { imageSrc: '', file: null }, // 1층
-        { imageSrc: '', file: null }  // 2층
-      ],
+      floorData: {
+        B1: { imageSrc: '', originalImageSrc: '', file: null, showingIcon: false },
+        F1: { imageSrc: '', originalImageSrc: '', file: null, showingIcon: false },
+        F2: { imageSrc: '', originalImageSrc: '', file: null, showingIcon: false },
+      },
       enlargedImage: null, // 확대된 이미지
-      showingIcons: [false, false, false],
-      presignedUrls: [], // 사전 서명된 URL (백엔드에서 제공)
     };
   },
   async created() {
-    try {
-      const response = await axios.get('http://15.164.246.244:8080/get-clubroom-images');
-      
-      if (response.data && response.data.images) {
-        this.floors = await Promise.all(
-          response.data.images.map(async (imageUrl, index) => {
-            const file = await this.urlToFile(imageUrl, `floor${index + 1}.jpg`);
-            return {
-              imageSrc: imageUrl,
-              file: file
-            };
-          })
-        );
-      }
-    } catch (error) {
-      console.error('이미지 불러오기 실패:', error);
-      alert('서버에서 이미지를 불러오는 데 실패했습니다.');
-    }
+    await this.fetchImages();
   },
   methods: {
-    async urlToFile(url, filename, mimeType = 'image/jpeg') {
-      const response = await fetch(url);
-      const blob = await response.blob();
-      return new File([blob], filename, { type: mimeType });
+    // 모든 층 이미지 가져오기
+    async fetchImages() {
+      const accessToken = store.state.accessToken;
+
+      try {
+        const floorIds = Object.keys(this.floorData);
+        const requests = floorIds.map((floorId) =>
+          axios
+            .get(`http://15.164.246.244:8080/admin/floor/photo/${floorId}`, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            })
+            .catch((error) => {
+              if (error.response && error.response.status === 404) {
+                console.warn(`층 ${floorId}에 대한 이미지를 찾을 수 없습니다.`);
+                return null; // 404 에러 무시
+              }
+              throw error;
+            })
+        );
+
+        const responses = await Promise.all(requests);
+
+        responses.forEach((response, index) => {
+          if (response && response.data && response.data.data) {
+            const floorId = Object.keys(this.floorData)[index];
+            this.floorData[floorId].imageSrc = response.data.data.presignedUrl;
+            this.floorData[floorId].originalImageSrc = response.data.data.presignedUrl; // 원본 저장
+          }
+        });
+      } catch (error) {
+        console.error('이미지 불러오기 실패:', error.response?.data || error);
+        alert('이미지를 불러오는 데 실패했습니다.');
+      }
     },
 
     // 파일 입력창 열기
-    triggerFileInput(index) {
-      const fileInputRef = this.$refs[`fileInput${index}`];
+    triggerFileInput(floorId) {
+      const fileInputRef = this.$refs[`fileInput${floorId}`];
       if (fileInputRef) fileInputRef.click();
     },
 
-    // 파일 업로드 처리
-    onImageUpload(index, event) {
+    // 파일 업로드 (클라이언트에서만 변경)
+    onImageUpload(floorId, event) {
       const file = event.target.files[0];
       if (!file) return;
 
@@ -183,31 +230,88 @@ export default {
 
       if (!validExtensions.includes(fileExtension) || file.size > maxFileSize) {
         alert('파일 형식이 잘못되었거나 크기가 초과되었습니다. (10MB 이하, png 또는 jpg만 허용)');
-        event.target.value = ''; // 동일 파일 선택 가능하도록 초기화
+        event.target.value = '';
         return;
       }
 
       const reader = new FileReader();
       reader.onload = (e) => {
-        this.floors[index].imageSrc = e.target.result; // 미리보기 이미지 설정
-        this.floors[index].file = file; // 업로드 파일 저장
+        this.floorData[floorId].imageSrc = e.target.result;
+        this.floorData[floorId].file = file;
       };
       reader.readAsDataURL(file);
 
-      // 동일 파일 선택 가능하도록 초기화
       event.target.value = '';
     },
 
-    // 이미지 삭제
-    deleteImage(index) {
-      this.floors[index].imageSrc = null; // 미리보기 이미지 제거
-      this.floors[index].file = null; // 파일 정보 제거
+    // 저장하기 버튼 클릭 시 서버에 저장
+    async saveImages() {
+      const accessToken = store.state.accessToken;
 
-      // <input> 요소 초기화
-      const fileInputRef = this.$refs[`fileInput${index}`];
-      if (fileInputRef) {
-        fileInputRef.value = ''; // 동일 파일 선택 가능
+      try {
+        const floorIds = Object.keys(this.floorData);
+
+        // 삭제된 이미지 서버에서 제거 (기존에 존재했던 이미지만 삭제 요청)
+        for (const floorId of floorIds) {
+          const { imageSrc, file, originalImageSrc } = this.floorData[floorId];
+
+          if (!imageSrc && !file && originalImageSrc) {
+            await axios.delete(`http://15.164.246.244:8080/admin/floor/photo/${floorId}`, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            });
+            console.log(`층 ${floorId} 이미지 삭제 성공`);
+          }
+        }
+
+        // 새 파일 업로드
+        for (const floorId of floorIds) {
+          const { file } = this.floorData[floorId];
+
+          if (file) {
+            const formData = new FormData();
+            formData.append('photo', file);
+
+            const response = await axios.put(
+              `http://15.164.246.244:8080/admin/floor/photo/${floorId}`,
+              formData,
+              {
+                headers: {
+                  Authorization: `Bearer ${accessToken}`,
+                  'Content-Type': 'multipart/form-data',
+                },
+              }
+            );
+
+            if (response.data && response.data.data.presignedUrl) {
+              const presignedUrl = response.data.data.presignedUrl;
+
+              await axios.put(presignedUrl, file, {
+                headers: {
+                  'Content-Type': file.type,
+                },
+              });
+
+              this.floorData[floorId].imageSrc = presignedUrl;
+              this.floorData[floorId].originalImageSrc = presignedUrl; // 업데이트된 URL 저장
+              console.log(`층 ${floorId} 이미지 저장 성공`);
+            }
+          }
+        }
+
+        alert('모든 이미지가 성공적으로 저장되었습니다.');
+        await this.fetchImages(); // 저장 후 이미지 갱신
+      } catch (error) {
+        console.error('이미지 저장 실패:', error.response?.data || error);
+        alert('이미지 저장에 실패했습니다.');
       }
+    },
+
+    // 이미지 삭제 (클라이언트에서만 변경)
+    markImageForDeletion(floorId) {
+      this.floorData[floorId].imageSrc = null;
+      this.floorData[floorId].file = null;
     },
 
     // 이미지 확대
@@ -221,58 +325,10 @@ export default {
     },
 
     // 아이콘 표시 토글
-    showIcons(index, status) {
-      this.showingIcons = [
-        ...this.showingIcons.slice(0, index),
-        status,
-        ...this.showingIcons.slice(index + 1)
-      ];
+    showIcons(floorId, status) {
+      this.floorData[floorId].showingIcon = status;
     },
-
-    // 이미지 저장
-    async saveImages() {
-      const formData = new FormData();
-
-      this.floors.forEach((floor, index) => {
-        if (floor.file) {
-          formData.append(`floor${index + 1}Image`, floor.file);
-        }
-      });
-
-      try {
-        const response = await axios.post('http://15.164.246.244:8080/save-clubroom-images', formData, {
-          headers: { 'Content-Type': 'multipart/form-data' }
-        });
-
-        // Presigned URL 제공되면 활용
-        if (response.data && response.data.presignedUrls) {
-          this.presignedUrls = response.data.presignedUrls;
-          await this.uploadToPresignedUrls();
-        }
-
-        alert('이미지가 저장되었습니다!');
-      } catch (error) {
-        alert('이미지 저장에 실패했습니다. 다시 시도해주세요.');
-      }
-    },
-
-    // Presigned URL로 이미지 업로드
-    async uploadToPresignedUrls() {
-      try {
-        await Promise.all(
-          this.presignedUrls.map(async (url, index) => {
-            if (this.floors[index].file) {
-              await axios.put(url, this.floors[index].file, {
-                headers: { 'Content-Type': this.floors[index].file.type }
-              });
-            }
-          })
-        );
-      } catch (error) {
-        alert('Presigned URL 업로드 실패');
-      }
-    }
-  }
+  },
 };
 </script>
 
