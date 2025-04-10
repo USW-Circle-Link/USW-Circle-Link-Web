41bbd7f0946e1f07e79ee0b80b67513900dfc47d
SungJae01 <miamo0426@gmail.com>
2025-04-11 01:01:12 +0900

    팝업창 메세지 수정

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -208,7 +208,7 @@ export default {
           this.showFailurePopup = true;
         }
         else {
-          this.failureMessage = "로그인 중 오류가 발생했습니다. 다시 시도해주세요....";
+          this.failureMessage = "로그인 중 오류가 발생했습니다. 문제가 지속되면 관리자에게 문의해주세요.";
           this.showFailurePopup = true;
         }
       }
