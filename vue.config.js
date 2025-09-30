5c3ef5dc072b971c33a0e26e96da3b3077523158
junse11 <izim5223@gmail.com>
2025-09-30 11:26:27 +0900

    경로 수정

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: '/'
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/USW-Circle-Link-Web/' 
+    : '/'
 })
