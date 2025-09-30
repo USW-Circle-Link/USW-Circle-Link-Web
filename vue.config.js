8f1a96b5f7dba2376aa5ff0582e344709aaff3d0
junse11 <izim5223@gmail.com>
2025-09-30 17:21:32 +0900

    라우터 변경

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === 'production' 
-    ? '/USW-Circle-Link-Web/' // ⬅️ 프로덕션 환경에서는 저장소 경로를 사용
+    ? '/USW-Circle-Link-Web/'
     : '/'
 })
